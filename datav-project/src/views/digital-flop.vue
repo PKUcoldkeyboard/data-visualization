ce71ac5d27a0cf4f40bfc91258d49a7c83fd3672
Elyar <1800016653@pku.edu.cn>
2021-04-17 13:13:37 +0800

    fixed some previous problems

--- datav-project/src/views/digital-flop.vue
+++ datav-project/src/views/digital-flop.vue
@@ -1,45 +1,74 @@
 <template>
-    <div>
-    	<div class="button" @click="changeConfig"><p>切换数据</p></div>
-        <dv-digital-flop :config="config" style="width:100px;height:50px;" />
-    </div>
+  <div>
+  	<div class="button" @click="changeConfig"><p>切换数据</p></div>
+    <br><br>
+    <dv-digital-flop :config="config" style="width:150px;height:75px;" />
+  </div>
 </template>
+
 <script>
 export default {
-    data() {
+  data() {
         return {
             config: {
                 number: [100],
                 content: '{nt}个'
             },
-            first: true
+            first: true,
+            second: false,
+            third:false
         }
     },
     methods: {
         changeConfig() {
             if (this.first) {
                 this.config = {
-                    number: [999],
+                    number: [555],
                     content: '{nt}个'
                 };
                 this.first = false;
-            } else {
+                this.second = true
+            } else if(this.second){
                 this.config = {
                     number: [100],
                     content: '{nt}个'
                 };
-                this.first = true;
+                this.second = false;
+                this.third = true
+            }
+            else if(this.third){
+                this.config = {
+                    number: [999],
+                    content: '{nt}个'
+                };
+                this.third = false;
+                this.first = true
             }
         }
+
+
     }
 }
+
 </script>
 <style>
-div {
+div{
 	margin:0 auto;
+  }
+
+.text{
+    font-size:24px;
+    width: 200px;
+    height: 80px;
+    color: #fff;
+    text-align: center;
+    vertical-align: middle;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
 }
 .button {
-	cursor: pointer;
+  cursor: pointer;
     font-size: 24px;
     width: 200px;
     height: 80px;
--- datav-project/src/views/scroll-board.vue
+++ datav-project/src/views/scroll-board.vue
@@ -1,11 +1,91 @@
 <template>
-  <div></div>
+  <div>
+  	<div class="text"><p>基本轮播表</p></div>
+  	<dv-scroll-board :config="config1" style="width:600px;height:300px" />
+  	<br><br>
+  	<div class="text"><p>加上表头和行号</p></div>
+  	<dv-scroll-board :config="config2" style="width:600px;height:300px" />
+  	<br><br>
+  	<div class="text"><p>整页轮播</p></div>
+  	<dv-scroll-board :config="config3" style="width:600px;height:300px" />
+
+  </div>
 </template>
 
 <script>
   export default{
+  data(){return {
+  config1:{
+  data: [
+    ['CZ6902', '北京', '长沙'],
+    ['CZ2090', '上海', '成都'],
+    ['CA3948', '乌鲁木齐', '北京'],
+    ['CA5435', '成都', '北京'],
+    ['CZ5642', '长沙', '贵州'],
+    ['MU8547', '北京', '乌鲁木齐'],
+    ['MU3757', '贵州', '成都'],
+    ['CA5946', '上海', '北京'],
+    ['CZ2847', '辽宁', '成都'],
+    ['MU5836', '乌鲁木齐', '上海']
+  ]
+  },
+
+  config2:{
+  header: ['航班号', '出发', '到达'],
+  data: [
+    ['CZ6902', '北京', '长沙'],
+    ['CZ2090', '上海', '成都'],
+    ['CA3948', '乌鲁木齐', '北京'],
+    ['CA5435', '成都', '北京'],
+    ['CZ5642', '长沙', '贵州'],
+    ['MU8547', '北京', '乌鲁木齐'],
+    ['MU3757', '贵州', '成都'],
+    ['CA5946', '上海', '北京'],
+    ['CZ2847', '辽宁', '成都'],
+    ['MU5836', '乌鲁木齐', '上海']
+  ],
+  index: true,
+  columnWidth: [50],
+  align: ['center']
+  },
+
+  config3:{
+  header: ['航班号', '出发', '到达'],
+  data: [
+    ['CZ6902', '北京', '长沙'],
+    ['CZ2090', '上海', '成都'],
+    ['CA3948', '乌鲁木齐', '北京'],
+    ['CA5435', '成都', '北京'],
+    ['CZ5642', '长沙', '贵州'],
+    ['MU8547', '北京', '乌鲁木齐'],
+    ['MU3757', '贵州', '成都'],
+    ['CA5946', '上海', '北京'],
+    ['CZ2847', '辽宁', '成都'],
+    ['MU5836', '乌鲁木齐', '上海']
+  ],
+  index: true,
+  columnWidth: [50],
+  align: ['center'],
+  carousel: 'page',
+  waitTime: 4000
+  }
+  }}
   }
 </script>
 <style>
-  
+div{
+	margin:0 auto;
+  }
+
+.text{
+    font-size:24px;
+    width: 200px;
+    height: 80px;
+    color: #fff;
+    text-align: center;
+    vertical-align: middle;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+} 
 </style>
